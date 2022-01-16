cc08d9fdc94464f31a0629d6f6da74ef6cd14a2c
srpnt-ls <60032233+srpnt-ls@users.noreply.github.com>
2022-01-16 15:46:32 +0300

    Changing punctuation

--- homework 6/homework6.js
+++ homework 6/homework6.js
@@ -1,28 +1,29 @@
 //Практическое 3
-function Cat (name) {
+function Cat(name) {
     var foodAmount = 0;
     this.name = name;
 
 
     function formatFoodAmount() {
-        return foodAmount +' гр.'
-    }
+        return foodAmount + ' гр.'
+    };
 
-    this.feed = function () {
+    this.feed = function() {
         console.log('Насыпаем в миску ' + formatFoodAmount() + ' корма.')
     };
 
     this.dailyNorm = function(amount) {
         if (!arguments.length) return formatFoodAmount();
         if (amount < 50) {
-            throw new Error("Коту будет мало");
+            throw new Error('Коту будет мало');
         }
         if (amount > 100) {
-            throw new Error("Кот объестся");
+            throw new Error('Кот объестся');
         }
         foodAmount = amount;
     }
 }
+
 var barsik = new Cat ('Barsik');
 barsik.dailyNorm(500);
 barsik.dailyNorm();
@@ -36,15 +37,15 @@ function Animal(name) {
     var self = this;
 
 
-    this.formatFoodAmount = function () {
+    this.formatFoodAmount = function() {
         return self._foodAmount + ' гр.'
     };
 
-    this.feed = function () {
+    this.feed = function() {
         console.log('Насыпаем в миску ' + self.formatFoodAmount() + ' корма.')
     };
 
-    this.dailyNorm = function (amount) {
+    this.dailyNorm = function(amount) {
         if (!arguments.length) return self.formatFoodAmount();
 
         if (amount < 50) {
@@ -62,18 +63,18 @@ function Cat(name) {
     var animalFeed = this.feed;
 
 
-    this.feed = function () {
+    this.feed = function() {
         animalFeed();
         console.log('Кот доволен');
         return this
     }
     
-    this.stroke = function () {
+    this.stroke = function() {
         console.log('Гладим кота');
         return this
     }
 }
 
 var barsik = new Cat('Барсик');
-barsik.dailyNorm(55)
-barsik.feed().stroke().feed().stroke().stroke()
+barsik.dailyNorm(55);
+barsik.feed().stroke().feed().stroke().stroke();
