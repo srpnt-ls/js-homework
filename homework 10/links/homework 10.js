5e26eba1f68e29d3df692d4c699de85205d06554
Liz Shersneva <shersneva.lisa@gmail.com>
2022-02-06 21:58:43 +0300

    Homework 10

---
+++ homework 10/links/homework 10.js
@@ -0,0 +1,44 @@
+var container = document.getElementsByTagName('div')[0],
+    newFirstParagraph = document.createElement('p'),
+    newSecondParagraph = document.createElement('p');
+
+newFirstParagraph.innerHTML = 'First Paragraph\n<a href="#">Link 1</a>\n<a href="#">Link 2</a>';
+newSecondParagraph.innerHTML = 'Second Paragraph\n<a href="https://nymag.com/">Link 3</a>\n<a href="https://www.instagram.com/">Link 4</a>';
+
+container.appendChild(newFirstParagraph);
+container.appendChild(newSecondParagraph);
+
+var mainButton = document.getElementsByTagName('button')[0];
+
+mainButton.onclick = function () {
+    var links1 = newFirstParagraph.getElementsByTagName('a');
+
+    for (var i = 0; i < links1.length; i++) {
+        links1[i].classList.add('red-links');
+    }
+}
+var links2 = newSecondParagraph.getElementsByTagName('a');
+
+for (var i = 0; i < links2.length; i++) {
+    links2[i].addEventListener('click', showLink)
+}
+
+//элемент задания из предыдущей домашки #10
+/*function showLink(event) {
+  event.preventDefault();
+  alert(this.getAttribute('href'));
+}*/
+
+function showLink() {
+    event.preventDefault();
+    console.log(this.innerHTML, {path: this.getAttribute('href')})
+    if (!localStorage.hasOwnProperty(this.innerHTML)) {
+        localStorage.setItem(this.innerHTML, JSON.stringify({path: this.getAttribute('href')}))
+        alert('Ссылка была сохранена!');
+        this.setAttribute('href', '#');
+    } else {
+        alert(JSON.parse(localStorage.getItem(this.innerHTML)).path);
+    }
+}
+
+localStorage.clear()
---
+++ homework 10/table/table-script.js
@@ -0,0 +1,28 @@
+var table = document.getElementsByTagName('table')[0];
+table.onclick = function (event) {
+    var target = event.target;
+
+    if (target.tagName !== 'TD' || target.className.indexOf('button') > -1) return;
+    target.innerHTML = '<input type = "text" value = "' + target.innerHTML + '" onblur = "saveText(this)">';
+    target.firstElementChild.focus();
+    target.firstElementChild.select();
+};
+
+function saveText(input) {
+    input.parentElement.innerHTML = input.value;
+}
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        document.activeElement.blur();
+    }
+});
+
+var newRowButton = document.getElementsByClassName('button')[0];
+
+newRowButton.onclick = function () {
+    var newRow = document.createElement('tr');
+    var referencePlace = table.getElementsByTagName('tr')[0];
+    referencePlace.parentNode.insertBefore(newRow, referencePlace);
+    newRow.innerHTML = '<td></td><td></td><td></td>'
+}
